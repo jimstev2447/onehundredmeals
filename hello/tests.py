df7383cf81dee2f1b0c71004ffd774e177c1fc44
Lauren Lewis <lorilew123@gmail.com>
2017-01-15 17:48:02 +0000

    Added first unit test and travis.yml and requirements

--- hello/tests.py
+++ hello/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+class ViewsTestCase(TestCase):
+
+    def testHello(self):
+        c = Client()
+        r = c.get('/hello')
+        self.assertIn('Screw you, world!', r.content)
+
