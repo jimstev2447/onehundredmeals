716b25ba7c28f4a16b8e14c18c92aba98fbe7f18
JimStevenson <jim.stevenson2447@gmail.com>
2017-01-23 21:17:53 +0000

    hot fixes

--- onehunredmeals/wsgi.py
+++ onehunredmeals/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for onehunredmeals project.
+WSGI config for onehundredmeals project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "onehundredmeals.settings")
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "onehundredmeals.settings")
